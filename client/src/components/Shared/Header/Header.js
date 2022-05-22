4e6dea22534ab3fb2f26b86d0061927ec9e2bca6
Vova <romaniuk.volodymyr.clg@chnu.edu.ua>
2022-05-22 11:54:36 +0300

    menushka done

--- client/src/components/Shared/Header/Header.js
+++ client/src/components/Shared/Header/Header.js
@@ -15,21 +15,60 @@ function Header() {
             </div>
          <div className="menu">
                   <ul className = "topmenu">
-                      <li>  <Link to="/">Головна</Link>
-                            <ul className="submenu">
-                                <li>gdhgdh</li>
-                                <li>gdhgdh</li>
-                                <li>gdhgdh</li>
+                      <li>  <Link to="/">Головна</Link></li>
+ 
+                      <li><Link to="/history">Історія</Link>
+                      <ul className="submenu">
+                                <li>Коледжу</li>
+                                <li>Університету</li>
+                            </ul></li>
+                      
+                      <li><Link to="/gfdg">Спеціальності</Link>
+                             <ul className="submenu">
+                                <li>Право</li>
+                                <li>Підприємство, торг та БД.</li>
+                                <li>Фінанси, БС та страхування</li>
+                                <li>Облік і оподаткування</li>
+                                <li>Прикладна математика</li>
+                                <li>Комп`ютерні науки</li>
+                                <li>Комп`ютерна інженерія</li>
                             </ul>
-                      </li>
-                      <li><Link to="/gfdg">Студенту</Link></li> 
-                      <li><Link to="/gdgdf">Чібєндія</Link></li>
-                      <li></li>
-                      <li></li>
+                        </li>
+                      <li><Link to="/gfdg">Студенту</Link>
+                          <ul className="submenu">
+                                <li>Розклад дзвінків</li>
+                                <li>Реквізити для оплати</li>
+                                <li>Курсова робота</li>
+                                <li>Допомога у пошуку роботи</li>
+                                <li>Стипендії</li>
+                                <li>Електронні підручники</li>
+                                <li>Силабуси дисциплін</li>
+                                <li>Оплата (on-line)</li>
+                                <li>Гуртожиток</li>
+                            </ul>
+                          </li>
+                        <li><Link to="/gfdg">Абітурієнту</Link>
+                            <ul className="submenu">
+                                <li>Підготовчі курси</li>
+                                <li>Програма підготовки</li>
+                                <li>Інформаціний пакет</li>
+                                <li>Правила прийому</li>
+                                <li>Додаткові бали</li>
+                                <li>Рейтингові списки(БЗСО)</li>
+                                <li>Рейтингові списки(ПЗСО)</li>
+                                <li>Накази про зарахування</li>
+                                <li>Вартість навчання</li>
+                                <li>Державне замовлення</li>
+                                <li>Мотиваційний лист</li>
+                                
+                            </ul>
+                        </li>
+                      <li><Link to="/">Адміністрація</Link></li>
+                      
                   </ul>
               </div>
-      </div>
-             
+      
+              </div>
         
       </div>
     );
