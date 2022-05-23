a77cc5e201915a9eb2e914908dec40dc21927de9
Vova <romaniuk.volodymyr.clg@chnu.edu.ua>
2022-05-23 16:29:19 +0300

    prosto

---
+++ client/src/components/MainPage/components/Durector.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Durector.css'
+const Durector = () => {
+  return (
+
+    <div className= "durector">
+        <div className="container">
+            <div className="info-durector"> 
+                <div className="header-durector">
+
+                </div>
+                <div className="images-durector">
+                    <div className="img-durector">
+
+                    </div>
+                    <div className="img-durector">
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+  )
+}
+
+export default Durector;
--- client/src/components/MainPage/mainPage.js
+++ client/src/components/MainPage/mainPage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import AboutInfo from './aboutInfo'
 import './mainPage.css'
 import Carousel from './components/Slider'
+import Durector from './components/Durector'
 const MainPage = () => {
   return (
-    <div className= "mainpage"><Carousel/></div>
+
+    <div className= "mainpage">
+      <Carousel/>
+      <Durector/>
+      <AboutInfo/>
+    </div>
+
   )
 }
 
