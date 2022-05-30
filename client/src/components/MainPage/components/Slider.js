764096f22c8c62090a0d677abdf625334421769d
Maks <lion20914king@gmail.com>
2022-05-30 21:02:02 +0300

    Slider Image refactor

--- client/src/components/MainPage/components/Slider.js
+++ client/src/components/MainPage/components/Slider.js
@@ -1,6 +1,5 @@
 import './Slider.css';
 import React, { useEffect ,useState  } from 'react'
-
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
@@ -20,7 +19,7 @@ function Carousel() {
     <div className="slider">
      <Slider  {...sliderSettings}> 
         <div className="element-slider">
-            <img src="https://i.ibb.co/k0mj4Q1/image.jpg" alt="" />
+            <img src="/slider1.jpg" alt="" />
             <div className="text-slider">
                 <h4>Найкращий коледж у місті</h4>
                 <h5>Коледж ЧНУ ім. Юрія Федьковича</h5>
@@ -28,7 +27,7 @@ function Carousel() {
             </div>
         </div>
         <div className="element-slider">
-            <img src="http://www.chnu.edu.ua/res//chnu.edu.ua/----------------------------------------------------------------------------------------------------14.05.20/book%20(5).jpeg" alt="" />
+            <img src="/book.jpeg" alt="" />
             <div className="text-slider">
                 <h4>Найбільший вибір спеціальностей</h4>
                 <h5>Ми чекаємо на тебе!!!</h5>
