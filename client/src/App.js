9793688798b7102b049d2cfefc00bbada2937fd1
Maks <lion20914king@gmail.com>
2022-06-23 18:30:16 +0300

    some done

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,15 @@ import { Route, Routes } from 'react-router';
 import MainPage from './components/MainPage/mainPage';
 import Header from './components/Shared/Header/Header';
 import Footer from './components/Shared/Footer/Footer';
-<<<<<<< HEAD
 import AdminPanel from './components/AdminPanel/AdminPanel';
 import React from 'react';
 import EditNewsItem from './components/AdminPanel/components/newsAdmin/EditNewsItem';
 import PagesEditor from './components/AdminPanel/SharedForAdmin/Editor';
 import PageEditor from './components/AdminPanel/components/pagesEditor/PageEditor';
 import Page from './components/Shared/UniversalPage/Page';
-=======
+
 import History from './components/HistoryPage/History';
->>>>>>> development
+
 
 function App() {
   return (
@@ -23,7 +22,7 @@ function App() {
         <Routes>
           <Route path ="/" exact element = {<MainPage/>}/>
           <Route path ="/admin" exact element = {<React.Fragment><AdminPanel/></React.Fragment>}/>
-          <Route path ="/admin/newsEdit/:section/:id" exact element = {<React.Fragment><EditNewsItem/></React.Fragment>}/>
+          <Route path ="/admin/newsEdit/:id" exact element = {<React.Fragment><EditNewsItem/></React.Fragment>}/>
           <Route path = "/admin/createNewPage/:section" exact element = {<React.Fragment><PageEditor/></React.Fragment>}/>
           <Route path = "/admin/editPage/:section/:id" exact element ={<React.Fragment><PageEditor/></React.Fragment>}/>
           <Route path = "/:section/:id" exact element ={<React.Fragment><Page/></React.Fragment>}/>
